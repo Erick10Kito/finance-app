140616cde4d5522665a642835a65c456bcefa903
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-01-11 14:07:04 -0300

    Added filter by month in the table

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import { ICategory } from "./types/Category";
 import { IItem } from "./types/Item";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
-import { useState } from "react";
-import { getCurrentMonth } from "./helpers/dateFilter";
+import { useEffect, useState } from "react";
+import { filterListByMonth, getCurrentMonth } from "./helpers/dateFilter";
+import { TableArea } from "./components/TableArea";
 
 export default function App() {
   const [list, setList] = useState(items); //Lista Geral, sem filtro nenhum, mostrando TUDO, independente do mes que foi adicionado
   const [filteredList, setFilteredList] = useState<IItem[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth()); //Lista do mes atual
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
   return (
     <div>
       <div className="bg-indigo-900 h-40 text-center">
         <h1 className="m-0 p-0 text-white pt-8">Sistema Financeiro</h1>
       </div>
-      <div className="m-auto mb-12 max-w-[980px]">...</div>
+      <div className="m-auto mb-12 max-w-[980px]">
+        <TableArea list={filteredList} />
+      </div>
     </div>
   );
 }
--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,7 +1,27 @@
 //TUDO RELACIONADO A MANIPULAÇÃO DE DATAS
 
+import { IItem } from "../types/Item";
+
 //Função abaixo serve para pegar o mes atual
 export const getCurrentMonth = () => {
   let now = new Date();
   return `${now.getFullYear()}-${now.getMonth() + 1}`;
+};
+
+//Função abaixo vai formular uma lista filtrada pelo mes, e list(sem estar filtrada) e date é o que essa função de lista filtrada receber inicialmente
+export const filterListByMonth = (list: IItem[], date: string) => {
+  let newList: IItem[] = [];
+  let [year, month] = date.split("-");
+
+  for (let i in list) {
+    //Esse if confere se a o ano e o mes do item é o mesmo que o atual
+    if (
+      list[i].date.getFullYear() === parseInt(year) &&
+      list[i].date.getMonth() + 1 === parseInt(month)
+    ) {
+      newList.push(list[i]);
+    }
+  }
+
+  return newList;
 };
